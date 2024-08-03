610a1cc4367e25e3907c3591a6a568bff6e7de03
chihieu1110 <chihieu20112001@gmail.com>
2024-08-03 11:42:00 +0700

    fix BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,8 @@
-export const API_ROOT = 'http://localhost:3007'
+let apiRoot = "";
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:3007";
+}
+if (process.env.BUILD_MODE === "production") {
+  apiRoot = "https://fsapp-backend.vercel.app/";
+}
+export const API_ROOT = apiRoot;
