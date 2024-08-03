a3fb281633f1f5d0b78a2fddf323c3845b31ae36
chihieu1110 <chihieu20112001@gmail.com>
2024-08-03 12:24:28 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ if (process.env.BUILD_MODE === "dev") {
   apiRoot = "http://localhost:3007";
 }
 if (process.env.BUILD_MODE === "production") {
-  apiRoot = "https://fsapp-backend.vercel.app";
+  apiRoot = "https://fsapp-backend.vercel.app/";
 }
 export const API_ROOT = apiRoot;
